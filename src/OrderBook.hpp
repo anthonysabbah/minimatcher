0a094db22ab667a949f933f47543663fe023ec17
Anthony Sabbah <exceptionless.professional.dev@gmail.com>
2023-04-01 13:48:55 +1030

    gotta store pointers of OrderBook for it to work

--- src/OrderBook.hpp
+++ src/OrderBook.hpp
@@ -1,10 +1,19 @@
-#include <functional>
-#include <vector>
-#include <unordered_map>
-#include <queue>
+#include "types.hpp"
 #include <cassert>
 #include <cmath>
-#include "types.hpp"
+#include <functional>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+
+#ifndef TRACE 
+#define TRACE 1
+#endif
+
+#ifndef DEBUG 
+#define DEBUG 1
+#endif
 
 #ifndef ORDERBOOK
 #define ORDERBOOK
@@ -19,7 +28,6 @@
 //   Sell
 // };
 
-
 // TODO: is this the best way to do this?
 // bool operator>(const Order& a, const Order& b){
 //   return a.qty > b.qty;
@@ -27,167 +35,195 @@
 
 bool constexpr is_bid(sprice_t const x) { return int32_t(x) >= 0; }
 
-class PriceLevel{
-  public:
-    sprice_t price;
-    std::vector<oid_t> orders;
 
-    PriceLevel(sprice_t _price){
-      price = _price;
-      orders = {};
-    }
+struct PriceLevel {
+  sprice_t price;
+  std::vector<oid_t> orders;
+
+  PriceLevel(sprice_t _price) {
+    price = _price;
+    orders = {};
+  }
 };
 
-class OrderBook{
+
+class OrderBook {
   private:
-  inline static oid_t latestOid;
-  inline static std::unordered_map<oid_t, Order*> orders;
+    inline static oid_t latestOid = oid_t(0);
+    inline static std::unordered_map<oid_t, Order *> orders;
 
-  std::vector<PriceLevel> sortedBuyPriceLevels;
-  std::vector<PriceLevel> sortedSellPriceLevels;
-
+    std::vector<PriceLevel> sortedBuyPriceLevels;
+    std::vector<PriceLevel> sortedSellPriceLevels;
 
   public:
-  static std::vector<OrderBook>* orderBooks;
+    inline static std::vector<OrderBook*> *orderBooks = new std::vector<OrderBook*>;
 
+    static oid_t getLatestOid() { return latestOid; }
+    size_t getBuyPriceLevelsSize() { return sortedBuyPriceLevels.size(); }
+    size_t getSellPriceLevelsSize() { return sortedSellPriceLevels.size(); }
+    std::vector<PriceLevel> getSortedSellPriceLevels() {return sortedSellPriceLevels;};
+    std::vector<PriceLevel> getSortedBuyPriceLevels() {return sortedBuyPriceLevels;};
 
-  static oid_t getLatestOid(){
-    return latestOid;
-  }
-
-  static void addOrder(Order* order){
+    static void addOrder(Order *order) {
+      order->oid = latestOid;
 #if TRACE
-    printf("ADD, %llu, %u, %u, %u\n", order->oid, order->securityId, order->qty, order->price);
+      printf("ADD, %llu, %u, %u, %i\n", order->oid, order->securityId, order->qty,
+          order->price);
 #endif
-    OrderBook orderBook = orderBooks->at(MKPRIMITIVE(order->securityId));
+      OrderBook *orderBook = orderBooks->at(MKPRIMITIVE(order->securityId));
 #if DEBUG
-    printf("ORDERBOOK FOUND\n");
+      printf("ORDERBOOK FOUND: %i\n", order->securityId);
 #endif
 
-    orderBook.ADD_ORDER(order);
-  }
-
-  void ADD_ORDER(Order* order){
-    orders[order->oid] = order;
-
-    std::vector<PriceLevel> *priceLevelsToAddTo = is_bid(order->price) ? 
-      &sortedBuyPriceLevels : 
-      &sortedSellPriceLevels;
-
-    auto insertionPoint = priceLevelsToAddTo->end(); // price level to insert new order at
-    bool found = false; // if appropriate price level has been found
-
-    order = MATCH_ORDER(order);
-
-    if(!order){
-      return;
+      orderBook->ADD_ORDER(order);
+      latestOid = oid_t(MKPRIMITIVE(latestOid) + 1);
     }
 
-    while(insertionPoint != priceLevelsToAddTo->begin()){
-      PriceLevel &toInsertAt = *insertionPoint;
+    void ADD_ORDER(Order *order) {
+      orders[order->oid] = order;
 
-      if(toInsertAt.price == order->price){
+      std::vector<PriceLevel> *priceLevelsToAddTo =
+        is_bid(order->price) ? &sortedBuyPriceLevels : &sortedSellPriceLevels;
+
+      auto insertionPoint =
+        priceLevelsToAddTo->end(); // price level to insert new order at
+      bool found = false;            // if appropriate price level has been found
+
+      // order = MATCH_ORDER(order);
+
+      if (!order) {
+        return;
 #if DEBUG
-        printf("FOUND PRICE LEVEL\n");
-#endif // TRACE
-        found = true;
-        toInsertAt.orders.push_back(order->oid);
+        printf("ORDER FILLED INSTANTLY\n");
+#endif 
       }
 
-    }
+      while (insertionPoint-- != priceLevelsToAddTo->begin()) {
+        PriceLevel &toInsertAt = *insertionPoint;
+#if DEBUG
+        printf("PRICE LEVEL: %i\n", toInsertAt.price);
+#endif
 
-    if(!found){
-      priceLevelsToAddTo->push_back(PriceLevel(order->price));
-      (*priceLevelsToAddTo)[priceLevelsToAddTo->size() - 1].orders.push_back(order->oid);
-    }
-
-
-    orders[order->oid] = order; // add order to orders map
-
-  }
-
-  static void cancelOrder(Order* order){
-    OrderBook orderBook = orderBooks->at(MKPRIMITIVE(order->securityId));
-
-    orderBook.CANCEL_ORDER(order);
-  }
-
-  // TODO: deleting may be slow (worst case is quadratic)
-  void CANCEL_ORDER(Order* order){
-    auto found = orders.find(order->oid);
-    if(found == orders.end()){
-      return;
-    }
-
-    std::vector<PriceLevel> *priceLevelsToSearch = is_bid(order->price) ? 
-      &sortedSellPriceLevels :
-      &sortedBuyPriceLevels;
-
-    orders.erase(order->oid);
-
-
-    auto insertionPoint = priceLevelsToSearch->end(); // price level to insert new order at
-
-    while(insertionPoint != priceLevelsToSearch->begin()){
-      PriceLevel &toInsertAt = *insertionPoint;
-
-      // erase orders if found price level
-      if(toInsertAt.price == order->price){
-        for(int i=0; i<toInsertAt.orders.size(); i++){
-          if((toInsertAt.orders[i]) == order->oid){
-            toInsertAt.orders.erase(toInsertAt.orders.begin() + i);
-          }
+        if (toInsertAt.price == order->price) {
+#if DEBUG
+          printf("FOUND PRICE LEVEL\n");
+#endif
+          found = true;
+          toInsertAt.orders.push_back(order->oid);
+          break;
+        } else if (order->price > toInsertAt.price) {
+        // insertion pt will be -1 if price < all prices
+        break;
         }
       }
 
+      if (!found) {
+        priceLevelsToAddTo->push_back(PriceLevel(order->price));
+        (*priceLevelsToAddTo)[priceLevelsToAddTo->size() - 1].orders.push_back(
+            order->oid);
+#if DEBUG
+        printf("CREATED PRICE LEVEL %i | LEVELS: %i\n", order->price, priceLevelsToAddTo->size());
+        printf("%i,%i\n", sortedBuyPriceLevels.size(), sortedSellPriceLevels.size());
+#endif
+      }
+
+      orders[order->oid] = order; // add order to orders map
     }
 
-  }
+    static void cancelOrder(Order *order) {
+#if TRACE
+      printf("CANCEL, %llu, %u, %u, %i\n", order->oid, order->securityId, order->qty,
+          order->price);
+#endif
+      OrderBook *orderBook = orderBooks->at(MKPRIMITIVE(order->securityId));
 
+      orderBook->CANCEL_ORDER(order);
+    }
 
-  Order* MATCH_ORDER(Order* order){
+    // TODO: deleting may be slow (worst case is quadratic)
+    void CANCEL_ORDER(Order *order) {
+      auto found = orders.find(order->oid);
+      if (found == orders.end()) {
+        return;
+      }
 
-    bool isBid = is_bid(order->price);
+      std::vector<PriceLevel> *priceLevelsToSearch =
+        is_bid(order->price) ? &sortedSellPriceLevels : &sortedBuyPriceLevels;
 
-    std::vector<PriceLevel> *priceLevelstoSearch =  is_bid(order->price)? 
-      &sortedBuyPriceLevels : 
-      &sortedSellPriceLevels;
-    
-    // TODO: is there a cleaner way to do this???
-    /*
-     * A comparator is used here to make the code look cleaner (otherwise, we'd have two while loops)
-     */
-    auto cmp = is_bid(order->price) ? 
-      [](sprice_t priceSearched, sprice_t orderPrice){
-        return std::abs(MKPRIMITIVE(priceSearched)) <= std::abs(MKPRIMITIVE(orderPrice));
-      } :
+      orders.erase(order->oid);
+
+      auto insertionPoint =
+        priceLevelsToSearch->end(); // price level to insert new order at
+
+      while (insertionPoint != priceLevelsToSearch->begin()) {
+        PriceLevel &toInsertAt = *insertionPoint;
+
+        // erase order if found price level
+        if (toInsertAt.price == order->price) {
+          for (int i = 0; i < toInsertAt.orders.size(); i++) {
+            if ((toInsertAt.orders[i]) == order->oid) {
+              toInsertAt.orders.erase(toInsertAt.orders.begin() + i);
+            }
+          }
+        }
+      }
+    }
+
+    Order *MATCH_ORDER(Order *order) {
+#if TRACE
+      printf("MATCH, %llu, %u, %u, %i\n", order->oid, order->securityId, order->qty,
+          order->price);
+#endif
+
+      bool isBid = is_bid(order->price);
+
+      std::vector<PriceLevel> *priceLevelstoSearch =
+        is_bid(order->price) ? &sortedBuyPriceLevels : &sortedSellPriceLevels;
+
+      // TODO: is there a cleaner way to do this???
+      /*
+       * A comparator is used here to make the code look cleaner (otherwise, we'd
+       * have two while loops)
+       */
+      auto cmp = is_bid(order->price) ?
+        [](sprice_t priceSearched, sprice_t orderPrice){
+          return std::abs(MKPRIMITIVE(priceSearched)) <= std::abs(MKPRIMITIVE(orderPrice));
+        } :
       [](sprice_t priceSearched, sprice_t orderPrice){
         return std::abs(MKPRIMITIVE(priceSearched)) >= std::abs(MKPRIMITIVE(orderPrice));
       };
 
-    auto priceLvlIt = priceLevelstoSearch->end();
-    
-    while(priceLvlIt-- != priceLevelstoSearch->begin() && cmp((*priceLvlIt).price, order->price)){
+      auto priceLvlIt = priceLevelstoSearch->end();
 
-      for (auto oid: (*priceLvlIt).orders){
-        qty_t fillAmount = orders[oid]->qty;
-        if(fillAmount >= order->qty){
-          orders[oid]->qty = (qty_t)(MKPRIMITIVE(orders[oid]->qty) - MKPRIMITIVE(order->qty)); // TODO: YUCK! FIX PLZ!
-          delete order;
-          return nullptr;
-        } else{
-          order->qty = (qty_t)(MKPRIMITIVE(order->qty) - MKPRIMITIVE(orders[oid]->qty)); // TODO: YUCK! FIX PLZ!
-          orders.erase(orders.find(oid));
+      while (priceLvlIt-- != priceLevelstoSearch->begin() &&
+          cmp((*priceLvlIt).price, order->price)) {
+
+        for (auto oid : (*priceLvlIt).orders) {
+          qty_t fillAmount = orders[oid]->qty;
+          if (fillAmount >= order->qty) {
+            orders[oid]->qty =
+              (qty_t)(MKPRIMITIVE(orders[oid]->qty) -
+                  MKPRIMITIVE(order->qty)); // TODO: YUCK! FIX PLZ!
+            delete order;
+            return nullptr;
+          } else {
+            order->qty =
+              (qty_t)(MKPRIMITIVE(order->qty) -
+                  MKPRIMITIVE(orders[oid]->qty)); // TODO: YUCK! FIX PLZ!
+            CANCEL_ORDER(orders[oid]);
+            orders.erase(orders.find(oid));
+          }
         }
       }
+      // #if TRACE
+      //     printf("MATCHED, %llu, %u, %u, %u\n", order->oid, order->securityId, order->qty,
+      //            order->price);
+      // #endif
+
+      return order;
     }
-
-    return order;
-  }
-
 };
-
-
-std::vector<OrderBook>* OrderBook::orderBooks = new std::vector<OrderBook>;
+ 
+// std::vector<OrderBook> *OrderBook::orderBooks = new std::vector<OrderBook>;
 
 #endif // !ORDERBOOK
--- test/OrderBook.test.cpp
+++ test/OrderBook.test.cpp
@@ -1,13 +1,84 @@
-#include <catch2/catch_test_macros.hpp>
+#include "catch.hpp"
 #include "OrderBook.hpp"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+TEST_CASE( "Testing OrderBook Methods", "[minimatcher]" ) {
+    Order order1;
+    order1.securityId = scrty_t(0);
+    order1.price = sprice_t(1000000);
+    order1.qty = qty_t(10);
+
+    Order order2;
+    order2.securityId = scrty_t(0);
+    order2.price = sprice_t(-1010000);
+    order2.qty = qty_t(10);
+
+    Order order3;
+    order3.securityId = scrty_t(0);
+    order3.price = sprice_t(1000000);
+    order3.qty = qty_t(5);
+
+    Order order4;
+    order4.securityId = scrty_t(0);
+    order4.price = sprice_t(-1010000);
+    order4.qty = qty_t(5);
+
+    Order order5;
+    order5.securityId = scrty_t(0);
+    order5.price = sprice_t(-1015000);
+    order5.qty = qty_t(10);
+
+    Order order6;
+    order6.securityId = scrty_t(0);
+    order6.price = sprice_t(990000);
+    order6.qty = qty_t(20);
+
+    OrderBook orderbk = OrderBook();
+
+    OrderBook::orderBooks->push_back(&orderbk);
+
+    OrderBook::addOrder(&order1);
+    OrderBook::addOrder(&order2);
+    OrderBook::addOrder(&order3);
+    OrderBook::addOrder(&order4);
+    OrderBook::addOrder(&order5);
+    OrderBook::addOrder(&order6);
+
+
+    OrderBook *orderBook0 = OrderBook::orderBooks->at(0);
+
+    REQUIRE( OrderBook::orderBooks->size() == 1 );
+    REQUIRE( orderBook0->getBuyPriceLevelsSize() == 2);
+    REQUIRE( orderBook0->getSellPriceLevelsSize() == 2);
+
+    WARN("CHECKING PRICE LEVEL ORDER");
+    for(auto priceLevel: orderBook0->getSortedBuyPriceLevels()){
+      WARN("" << int32_t(priceLevel.price));
+    }
+    WARN("-----");
+    for(auto priceLevel: orderBook0->getSortedSellPriceLevels()){
+      WARN("" << int32_t(priceLevel.price));
+    }
+
+
+    // SECTION( "resizing bigger changes size and capacity" ) {
+    // }
+    // SECTION( "resizing smaller changes size but not capacity" ) {
+    // }
+    // SECTION( "reserving bigger changes capacity but not size" ) {
+    // }
+    // SECTION( "reserving smaller does not change size or capacity" ) {
+    // }
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
+
+// TEST_CASE( "Test Orderbook", "[minimatcher]" ) {
+//   Order order;
+//   order.oid = oid_t(69);
+//   order.securityId = scrty_t(0);
+//   order.price = sprice_t(4206969);
+//   order.qty = qty_t(42);
+
+//   OrderBook orderbk = OrderBook();
+//   OrderBook::orderBooks->push_back(orderbk);
+//   OrderBook::addOrder(&order);
+// }
